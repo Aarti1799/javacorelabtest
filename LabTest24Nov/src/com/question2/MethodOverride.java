4dd92b622c21650789340f24b2ece7c8a6ac3256
Aarti1799 <115772657+Aarti1799@users.noreply.github.com>
2022-11-25 12:38:24 +0530

    Update MethodOverride.java

--- LabTest24Nov/src/com/question2/MethodOverride.java
+++ LabTest24Nov/src/com/question2/MethodOverride.java
@@ -2,18 +2,11 @@ package com.question2;
 
 public class MethodOverride {
 	public static void main(String[] args) {
-		Inheritance in= new Inheritance();
-		DerivedClass dc = new DerivedClass();
+	
 		DerivedClass dc = new DerivedClass();
 		dc.display1();
 		dc.display();
-		/*Inheritance ref;
-		ref = in;
-		ref.display();
 		
-		DerivedClass ref1;
-		ref1=dc;
-		ref1.display();*/
 	}
 
 }
